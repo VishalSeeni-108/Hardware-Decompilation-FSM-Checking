78189f9766c4fc0c2f5d5f5b0bd19241766c93b1
Vishal S <vishalseeni@gmail.com>
2024-07-29 01:57:02 -0700

    Beginning of register loop searching algo

--- extract_fsm.py
+++ extract_fsm.py
@@ -1,5 +1,6 @@
 import pyrtl
 import pyparsing
+from collections import deque
 
 with open("register_test.blif", "r") as f:
     blif = f.read()
@@ -15,6 +16,32 @@ pyrtl.importexport.input_from_blif(blif, merge_io_vectors=False, clock_name='clk
 #         print("Inputs: " + str(x.args))
 #         print("Outputs: " + str(x.dests))
 
-for y in pyrtl.visualization.net_graph():
-    print(y)
 
+for log_net in pyrtl.visualization.net_graph():
+    print(log_net)
+    #Search for registers
+    if(hasattr(log_net, 'op') and log_net.op == 'r' and hasattr(log_net, 'dests')):
+    #Perform DFS to find loops
+            #Queue destinations
+            destinations = deque()
+            for dest in log_net.dests:
+                for pot_dest in pyrtl.visualization.net_graph():
+                    if(hasattr(pot_dest, 'op') and hasattr(pot_dest, 'args')):
+                        for inputs in pot_dest.args:
+                            if(inputs.name == dest.name):
+                                 destinations.append(pot_dest)
+                                 break; 
+
+            for destination in destinations: 
+                 print("Destination: ", destination)
+
+    #Find netlists where dests are inputs
+
+#Find a register
+#Perform DFS from register to find loops
+#Repeat for every register 
+
+                
+
+#Note difference between a register *operation* (copies argument to destination) and register *objects* (i.e. WireVector objects)
+#For our purposes, every register operation should have a register destination, so we can search for registers by searching for register operations
