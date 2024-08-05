77676519a671369791b7427fdde2c927909ae1ee
Viz108 <67886538+Viz108@users.noreply.github.com>
2024-08-04 19:32:00 -0700

    Work on loop search - need to fix queue issue

--- extract_fsm.py
+++ extract_fsm.py
@@ -21,7 +21,8 @@ for log_net in pyrtl.visualization.net_graph():
     print(log_net)
     #Search for registers
     if(hasattr(log_net, 'op') and log_net.op == 'r' and hasattr(log_net, 'dests')):
-    #Perform DFS to find loops
+            print("Register Found")
+            #Perform DFS to find loops
             #Queue destinations
             destinations = deque()
             for dest in log_net.dests:
@@ -29,11 +30,39 @@ for log_net in pyrtl.visualization.net_graph():
                     if(hasattr(pot_dest, 'op') and hasattr(pot_dest, 'args')):
                         for inputs in pot_dest.args:
                             if(inputs.name == dest.name):
+                                 print("Adding destination: ", pot_dest)
                                  destinations.append(pot_dest)
                                  break; 
 
-            for destination in destinations: 
-                 print("Destination: ", destination)
+            #Loop through destinations and search for further branches to explore. Make sure to add already
+            #visited nodes to visited list
+            visited = []
+            loop_found = False
+            for destination in destinations: #TODO - Need to fix recursive looping, Python does not allow items to be queued while iterating over the same deque
+                print("Searching destination: ", destination)
+                for dest in destination.dests:
+                    #Process node - check if any of the destinations of the current node is an input to the original register
+                    for input in log_net.args:
+                        if(dest.name == input.name):
+                            print("Loop found - "  "this register" + " is a potential state register") #Need to find a way to print register name
+                            loop_found = True
+                            break;            
+                    if(loop_found):
+                        break; 
+
+                if(loop_found): 
+                    break; 
+
+                #Queue for searching
+                for pot_dest in pyrtl.visualization.net_graph():
+                    if(hasattr(pot_dest, 'op') and hasattr(pot_dest, 'args')):
+                        for inputs in pot_dest.args:
+                            if(inputs.name == dest.name):
+                                #Check for loop
+                                if(not (pot_dest in visited)):
+                                    destinations.append(pot_dest)
+                                    break; 
+
 
     #Find netlists where dests are inputs
 
